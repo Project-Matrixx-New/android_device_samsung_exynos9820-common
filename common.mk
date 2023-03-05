76d4e82da40de1e8bf49abdcb63394566ecdee97
pix106 <sbordenave@gmail.com>
2023-03-05 08:59:07 +0100

    exynos9820-common: Drop schedtune
    * Kernel supports Uclamp
    
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>

--- common.mk
+++ common.mk
@@ -118,7 +118,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/init.exynos9820.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos9820.rc \
     $(COMMON_PATH)/configs/init/init.exynos9820.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos9820.usb.rc \
     $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsung.rc \
-    $(COMMON_PATH)/configs/init/init.stune.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.stune.rc \
     $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
 ifeq ($(TARGET_HAVE_FOD),true)
